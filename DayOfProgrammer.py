4859ecd9e287ffbc08a9f47569d5de4340eea90d
Phuc Thai Hong <phucthaihong@gmail.com>
2020-07-16 12:30:31 +1000

    SOLVED Day of the Programmer
    https://www.hackerrank.com/challenges/day-of-the-programmer/problem

---
+++ DayOfProgrammer.py
@@ -0,0 +1,7 @@
+def dayOfProgrammer(year):
+    if year == 1918:
+        return ('26.09.1918')
+    elif ((year < 1918) & (year % 4 == 0)) | ((year > 1918) & ((year % 400 == 0) | ((year % 4 == 0) & (year % 100 != 0)))):
+        return('12.09.' + str(year))
+    else:
+        return('13.09.' + str(year))
