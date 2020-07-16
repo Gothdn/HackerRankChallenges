0d8c252b94e466ed4b843a73bb41997ce931af67
Phuc Thai Hong <phucthaihong@gmail.com>
2020-07-16 12:49:46 +1000

    SOLVED Picking Numbers
    https://www.hackerrank.com/challenges/picking-numbers/problem

---
+++ PickingNumbers.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    count = [0 for x in range(0, 101)]
+    for num in a:
+        count[num] += 1
+    result = 0
+    for i in range(2, 101):
+        result = max([result, count[i] + count[i - 1]])
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
