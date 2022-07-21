e31d597f5a2d7f714601bf4c0959e3466f57e1ed
Phuc Thai <phucthaihong@gmail.com>
2022-07-21 21:46:13 +1000

    Create CountingValleys.js
    SOLVED
    https://www.hackerrank.com/challenges/counting-valleys/problem

---
+++ CountingValleys.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+    let height = 0;
+    let count = 0;
+    for (let i = 0; i < steps; i++) {
+        if (path[i] == "U") {
+            height++;
+            if (height == 0) {
+                count++;
+            }
+        } else if (path[i] == "D") {
+            height--;
+        }
+    }
+    
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const steps = parseInt(readLine().trim(), 10);
+
+    const path = readLine();
+
+    const result = countingValleys(steps, path);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
