f475a2c07f43b9a44b41868be82fbe1f9205de36
Phuc Thai <phucthaihong@gmail.com>
2022-09-16 22:43:58 +1000

    Create SparseArrays.js
    SOLVED
    https://www.hackerrank.com/challenges/sparse-arrays/problem

---
+++ SparseArrays.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY stringList
+ *  2. STRING_ARRAY queries
+ */
+
+function matchingStrings(stringList, queries) {
+    // Write your code here
+    let res = "";
+    for (let q = 0; q < queries.length; q++) {
+        let count = 0;
+        for (let i = 0; i < stringList.length; i++) {
+            if (stringList[i] == queries[q]) {
+                count++;
+            }
+        }
+        res = res + count + "\n";
+    }
+    return res;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const stringListCount = parseInt(readLine().trim(), 10);
+
+    let stringList = [];
+
+    for (let i = 0; i < stringListCount; i++) {
+        const stringListItem = readLine();
+        stringList.push(stringListItem);
+    }
+
+    const queriesCount = parseInt(readLine().trim(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = readLine();
+        queries.push(queriesItem);
+    }
+
+    const res = matchingStrings(stringList, queries);
+
+    ws.write(res);
+
+    ws.end();
+}
