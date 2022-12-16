901b675cba3fc7056bc1f6627ddf3bd6992bcadc
P Thai <phucthaihong@gmail.com>
2022-12-16 12:15:39 +1100

    Create NonDivisibleSubset.js
    SOLVED
    https://www.hackerrank.com/challenges/non-divisible-subset

---
+++ NonDivisibleSubset.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'nonDivisibleSubset' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY s
+ */
+
+function nonDivisibleSubset(k, s) {
+    let c = new Array(k).fill(0);
+    s.forEach(e => c[e%k]++);
+    let res = 0;
+    for (let i = 1; i < k / 2; i++) {
+        res += Math.max(c[i], c[k - i]);
+    }
+    if (k % 2 == 0) {
+        res += Math.min(c[k / 2], 1);
+    }
+    res += Math.min(c[0], 1);
+    //console.log(c);
+    return res;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const result = nonDivisibleSubset(k, s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
